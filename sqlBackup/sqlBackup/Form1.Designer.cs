a3d928cb98ba046a6cfaa814b032faf226f8a377
WonderlustKing <chrisban4@gmail.com>
2015-11-08 15:28:55 +0200

    Some text changes

--- sqlBackup/sqlBackup/Form1.Designer.cs
+++ sqlBackup/sqlBackup/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -40,14 +41,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Connection form here";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(705, 354);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Connect";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +65,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
