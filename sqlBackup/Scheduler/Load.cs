fb4d64e6ae49cf925a111c55b7d4a4196d929abc
basilisxatzis1994 <basilis.euosmos@gmail.com>
2015-12-07 23:20:02 +0200

    ScheduleLoad
    Allages sth load fia na mporei na diabasei apo to arxeio

--- sqlBackup/Scheduler/Load.cs
+++ sqlBackup/Scheduler/Load.cs
@@ -8,121 +8,210 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.Collections.Generic;
+using sqlBackup;
 namespace BackUpDb
 {
     class Load
     {
+
+        private String time;
         private String hostname;
         private String port;
         private String username;
         private String password;
+        private String ftphostname;
         private String ftpusername;
         private String ftppassword;
+        private String email;
         private List<String> dbBackUp = new List<string>();
-        StringBuilder stringsave = new StringBuilder();
+
+        StringBuilder stringsave = null;
+
         public Load(/*String hostname, String port, String username, String password*/)
         {
-            
+
             //setHostname(hostname, port);
             //setUsername(username);
             //setPassword(password);
 
         }
+
+        //set mathodoi
+        public void setTime(String time)
+        {
+            this.time = time;
+        }
+
         public void setHostname(String hostname, String port)
         {
             this.hostname = hostname;
             this.port = port;
             stringsave.Append(this.hostname + "\n" + this.port + "\n");
         }
+
         public void setUsername(String username)
         {
             this.username = username;
             stringsave.Append(this.username + "\n");
         }
+
         public void setPassword(String password)
         {
             this.password = password;
             stringsave.Append(this.password);
         }
+
+        public void setFtpHostname(String ftphostname)
+        {
+            this.ftphostname = ftphostname;
+        }
+
+        public void setFtpusername(String ftpusername)
+        {
+            this.ftpusername = ftpusername;
+        }
+
+        public void setFtppassword(String ftppassworde)
+        {
+            this.ftppassword = ftppassworde;
+        }
+
+        public void setEmail(String email)
+        {
+            this.email = email;
+        }
+
+        public void setbackup(String backname)
+        {
+            this.dbBackUp.Add(backname);
+        }
+
+        //get methodoi
+
+        public String getTime()
+        {
+            return time;
+        }
+
         public String getHostname()
         {
 
             return this.hostname;
         }
+
         public String getPort()
         {
 
             return this.port;
         }
+
         public String getUsername()
         {
 
             return this.username;
         }
+
         public String getPassword()
         {
 
             return this.password;
         }
-        public void setFtpusername(String ftpusername)
+
+        public String getFtphostname()
         {
-            this.ftpusername = username;
+            return this.ftphostname;
         }
-        public void setFtppassword(String ftppassworde)
-        {
-            this.ftppassword = ftppassworde;
-        }
+
         public String getFtpusername()
         {
             return this.ftpusername;
         }
-        public String getFtppassworde()
+
+        public String getFtppassword()
         {
             return this.ftppassword;
         }
-        public void setbackup(String backname)
+
+        public String getEmaiil()
         {
-            this.dbBackUp.Add(backname);
+            return email;
         }
+
         public String[] getbackup()
         {
             return this.dbBackUp.ToArray();
         }
+
         override
         public String ToString()
         {
             return Convert.ToString(stringsave);
-        } 
-        TcpClient tcpclnt = new TcpClient();
+        }
+
+        TcpClient tcpclnt = null;
+
         public void LoadFromFile()
         {
             try
             {
-                OpenFileDialog openfile = new OpenFileDialog();
-                openfile.ShowDialog();
-                String filename = openfile.FileName;
-                StreamReader readfromLoad = new StreamReader(filename);
-                ConnectToServer connection = new ConnectToServer();
-                //connection.setHostname(readfromLoad.ReadLine(), readfromLoad.ReadLine());
-                //connection.setUsername(readfromLoad.ReadLine());
-                //connection.setPassword(readfromLoad.ReadLine());
-                setHostname(readfromLoad.ReadLine(), readfromLoad.ReadLine());
-                setUsername(readfromLoad.ReadLine());
-                setPassword(readfromLoad.ReadLine());
-                //setFtpHostname(readfromLoad.ReadLine());
-                setFtpusername(readfromLoad.ReadLine());
-                setFtppassword(readfromLoad.ReadLine());
-                int i = 0;
-                
-                do {
-                    setbackup(readfromLoad.ReadLine());
-                    i++;
-                        } while (!(readfromLoad.EndOfStream));
-            
-                tcpclnt.Connect(this.hostname, Convert.ToInt32(this.port));
-            }catch(Exception ex)
+
+                string schedulefile = null;
+
+                //orismos tou path pou einai to arxeio p tha diabazoume
+                string[] dirs = Directory.GetFiles(@"C:\\Users\\Bill\\Documents\\GitHubVisualStudio\\sqlBackup\\sqlBackup\\sqlBackup\\ScheduleFile\\");
+                //trexei gia osa arxeia exei o fakelos
+                foreach (string dir in dirs)
+                {
+
+                    stringsave = new StringBuilder();
+                    string[] pin = getbackup();
+
+                    for (int k = 0; k < pin.Length; k++)
+                    {
+                        Console.WriteLine(pin[k].ToString());
+                    }
+
+                    schedulefile = dir;
+                    tcpclnt = new TcpClient();
+                    //anoigma tou areiou
+                    StreamReader readfromLoad = new StreamReader(schedulefile);
+                    ConnectToServer connection = new ConnectToServer();
+                    
+                    //diabasma apo arxeio
+                    setTime(readfromLoad.ReadLine());
+                    setHostname(readfromLoad.ReadLine(), readfromLoad.ReadLine());
+                    setUsername(readfromLoad.ReadLine());
+                    setPassword(readfromLoad.ReadLine());
+                    setFtpHostname(readfromLoad.ReadLine());
+                    setFtpusername(readfromLoad.ReadLine());
+                    setFtppassword(readfromLoad.ReadLine());
+                    setEmail(readfromLoad.ReadLine());
+
+                    int i = 0;
+                    {
+
+                        setbackup(readfromLoad.ReadLine());
+                        i++;
+
+                    } while (!(readfromLoad.EndOfStream)) ;
+
+                    for (int k = 0; k < pin.Length; k++)
+                    {
+                        Console.WriteLine(pin[k].ToString());
+                    }
+
+                    tcpclnt.Connect(hostname, Convert.ToInt32(port));
+                    if (tcpclnt.Connected)
+                    {
+                        BackupDb back = new BackupDb(getHostname(), getUsername(), getPassword(), getbackup(), @"C:\Users\Bill\AppData\Roaming\sqlbackup\", getFtphostname(), getFtpusername(), getFtppassword());
+                        back.downloadDb();
+                        tcpclnt.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.StackTrace);
             }
         }
         public Boolean Connected()
@@ -130,5 +219,5 @@ namespace BackUpDb
             return tcpclnt.Connected;
         }
     }
- }
+}
 
